b81302e2a08fbbd5bee6730b69f85e595212976a
Sophie Obomighie <sophiecodes@gmail.com>
2024-12-23 08:57:08 +0100

    initialize snake and food and generate randome food position

--- SnakeGame.py
+++ SnakeGame.py
@@ -22,3 +22,18 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #setting up the clock
 clock = pygame.time.Clock()
+
+# snake and food initialization
+snake_pos = [[WIDTH//2, HEIGHT//2]]
+snake_speed = [0, BLOCK_SIZE]
+
+teleport_walls = True #set to true to enable Wall teleporting
+
+# generate food position
+def generate_food():
+    while True:
+        x = random.randint(0, (WIDTH - BLOCK_SIZE) // BLOCK_SIZE ) * BLOCK_SIZE
+        y = random.randint(0, (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        food_pos = [x, y]
+        if food_pos not in snake_pos:
+            return food_pos
